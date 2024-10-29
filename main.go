afd8fa0db240c786473417e70f75acaa084fa951
Heslimited <dimzo333@gmail.com>
2024-10-29 17:38:55 +0300

    Added PATCH and DELETE

--- main.go
+++ main.go
@@ -8,25 +8,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type requestBody struct {
-	Text string `json:"message"`
-}
-
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	var body requestBody
+	var body Message
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 
-	newMessage := Message{Text: body.Text}
-	DB.Create(&newMessage)
+	DB.Create(&body)
 
 	// message = body.Message
 	w.WriteHeader(http.StatusOK)
@@ -44,7 +39,56 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(messages)
-	// fmt.Fprintf(w, "Hello, %s", message)
+}
+
+func patchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPatch {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var body Message
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	var message Message
+	if err := DB.First(&message, id).Error; err != nil {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+
+	message.Text = body.Text
+	DB.Save(&message)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Message updated: %s", body.Text)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var body Message
+	if err := DB.First(&body, id).Error; err != nil {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+
+	DB.Delete(&body)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Message deleted")
 }
 
 func main() {
@@ -58,6 +102,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/post", postHandler).Methods("POST")
 	router.HandleFunc("/api/get", getHandler).Methods("GET")
+	router.HandleFunc("/api/patch/{id}", patchHandler).Methods("PATCH")
+	router.HandleFunc("/api/delete/{id}", deleteHandler).Methods("DELETE")
 
 	// Запускаем сервер на порту 8080
 	http.ListenAndServe(":8080", router)
